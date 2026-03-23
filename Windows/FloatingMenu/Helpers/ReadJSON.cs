b695150b87aa72598c57eeafae421f3a56ef8665
Namitha <namithax.gangadhar@intel.com>
2026-03-23 15:59:53 +0530

    Integrated TouchDataCapture.exe to Floating Menu

---
+++ Windows/FloatingMenu/Helpers/ReadJSON.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Ports;
+using System.Text;
+using System.Text.Json;
+
+namespace FloatingMenu.Helpers
+{
+    internal static class ReadJSON
+    {
+        public static string GetPortFromExternalConfig()
+        {
+            string path = @"C:\Floating_Menu\config.json";
+
+           
+                if (!File.Exists(path))
+                    throw new Exception("Config file not found");
+
+                string json = File.ReadAllText(path);
+
+                ConfigModel config;
+                try
+                {
+                    config = JsonSerializer.Deserialize<ConfigModel>(json);
+                }
+                catch (JsonException ex)
+                {
+                    throw new Exception("Invalid JSON format: " + ex.Message);
+                }
+
+                if (config == null)
+                    throw new Exception("Invalid config structure");
+
+                if (string.IsNullOrWhiteSpace(config.Port))
+                    throw new Exception("Port is missing in config");
+
+                string port = config.Port.Trim().ToUpper();
+
+                var availablePorts = SerialPort.GetPortNames();
+
+                if (!availablePorts.Contains(port))
+                    throw new Exception($"Port '{port}' not found on this machine");
+
+                return port;
+           
+        }
+     
+    }
+  
+    class ConfigModel
+    {
+        public string Port { get; set; }
+    }
+}
--- Windows/FloatingMenu/MainWindow.xaml.cs
+++ Windows/FloatingMenu/MainWindow.xaml.cs
@@ -4,7 +4,9 @@
 *******************************************************************************/
 
 using FloatingMenu.Controls;
+using FloatingMenu.Helpers;
 using System.Diagnostics;
+using System.IO.Ports;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
@@ -34,7 +36,7 @@ namespace FloatingMenu
         private Process _annotationProcess;
 
         private SignalSource _signalSourcePage;
-
+        private Process _touchProcess;
         private bool _cameraClosedManually = false;
         public MainWindow()
         {
@@ -137,7 +139,7 @@ namespace FloatingMenu
             {
                 _cameraWindow.Close();
                 _cameraWindow = null;
-                EdgeMenu.PCStatus.Text = _signalSourcePage.UpdateDevice();
+                KillTouchDataCapture();
             }
         }
 
@@ -176,7 +178,7 @@ namespace FloatingMenu
             }
         
             ShowFlyout(_signalSourcePage);
-            EdgeMenu.PCStatus.Text = _signalSourcePage.UpdateDevice();
+           
         }
 
         private void FlyoutPage_Loaded(object sender, RoutedEventArgs e)
@@ -214,6 +216,7 @@ namespace FloatingMenu
 
         private void OpenCameraWindow(int index)
         {
+            
             if (index == -1)
             {
                 CloseCameraWindow();
@@ -221,40 +224,140 @@ namespace FloatingMenu
             }
             if (_cameraWindow != null)
                 return;
-
+            if (!LaunchTouchDataCapture())
+            {
+                if (_signalSourcePage != null)
+                {
+                    foreach (var device in _signalSourcePage.Devices)
+                    {
+                        if (device.Status == DeviceStatusEnum.Connected)
+                        {
+                            device.Status = DeviceStatusEnum.Available;
+                        }
+                    }
+                }
+                return;
+            }
             _cameraWindow = new CameraWindow(index);
             _cameraWindow.CameraClosed += OnCameraClosed;
 
             _cameraWindow.Show();
            
+           
             CollapseMenu();
-            EdgeMenu.PCStatus.Text = _signalSourcePage.UpdateDevice();
+           
         }
 
+        private bool LaunchTouchDataCapture()
+        {
+            if (_touchProcess != null && !_touchProcess.HasExited)
+                return false;
+
+            try
+            {
+                string port = Helpers.ReadJSON.GetPortFromExternalConfig();
+
+                string exePath = System.IO.Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Services", "TouchDataCapture",
+                "TouchDataCaptureService.exe"
+            );
+
+                _touchProcess = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = exePath,
+                        Arguments = $"--port {port}",
+                        WorkingDirectory = System.IO.Path.GetDirectoryName(exePath),
+                        UseShellExecute = true
+                    }
+                };
+
+                _touchProcess.Start();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                var ports = string.Join(", ", SerialPort.GetPortNames()); //
+                MessageBox.Show(
+                    $"Invalid port in config\n\nAvailable ports:\n{ports}.",
+                    "Configuration Error",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                return false;
+            }
+        }
 
         private void OnCameraClosed()
         {
+
             Dispatcher.Invoke(() =>
             {
                 _cameraWindow = null;
-                _cameraClosedManually = true;
                 _menuOpen = false;
+
+                if (_signalSourcePage != null)
+                {
+                    foreach (var device in _signalSourcePage.Devices)
+                    {
+                        if (device.Status == DeviceStatusEnum.Connected)
+                        {
+                            device.Status = DeviceStatusEnum.Available;
+                        }
+                    }
+                }
+
                 ToggleMenu();
                 EdgeMenu.ClearSelection();
                 EdgeMenu.SelectMenuItem(2);
-                if (_cameraClosedManually)
-                {
-                    _signalSourcePage = new SignalSource();
-                    _signalSourcePage.DeviceSelected += OpenCameraWindow;
-                    _cameraClosedManually = false;
-                }
-
+                KillTouchDataCapture();
                 ShowFlyout(_signalSourcePage);
-                EdgeMenu.PCStatus.Text = _signalSourcePage.UpdateDevice();
+                EdgeMenu.PCStatus.Text = "";
             });
+
+
+
+            //Dispatcher.Invoke(() =>
+            //{
+            //    _cameraWindow = null;
+            //    _cameraClosedManually = true;
+            //    _menuOpen = false;
+            //    ToggleMenu();
+            //    EdgeMenu.ClearSelection();
+            //    EdgeMenu.SelectMenuItem(2);
+            //    if (_cameraClosedManually)
+            //    {
+            //        _signalSourcePage = new SignalSource();
+            //        _signalSourcePage.DeviceSelected += OpenCameraWindow;
+            //        _cameraClosedManually = false;
+            //    }
+            //    KillTouchDataCapture();
+            //    ShowFlyout(_signalSourcePage);
+
+            //});
         }
 
-       
+        private void KillTouchDataCapture()
+        {
+            try
+            {
+                if (_touchProcess != null && !_touchProcess.HasExited)
+                {
+                    _touchProcess.Kill();
+                    _touchProcess.WaitForExit();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error killing process: " + ex.Message);
+            }
+            finally
+            {
+                _touchProcess = null;
+            }
+        }
+
         private void LaunchAnnotationAppAsync()
         {
             try
